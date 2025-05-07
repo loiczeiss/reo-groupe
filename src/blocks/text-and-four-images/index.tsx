5089dd549bad376f28a73ff832a6511a7e95d850
Loïc <loiczeiss@gmail.com>
2025-05-07 12:17:03 +0200

    feat(general): add responsiveness -  made textAndFourImages responsive

--- src/blocks/text-and-four-images/index.tsx
+++ src/blocks/text-and-four-images/index.tsx
@@ -18,37 +18,37 @@ interface TextAndFourImagesProps {
 export function TextAndFourImagesBlock(props: TextAndFourImagesProps) {
   const { heading, description, subheading1, subcontent1, subheading2, subcontent2, images } = props
   return (
-    <div className={'w-full h-screen flex px-12 py-16'}>
-      <div className={'w-1/2 flex flex-col items-center space-y-6'}>
-        <div className={'flex flex-col space-y-4'}>
-          <h1 className={'text-5xl'}>{heading}</h1>
-          <p className={'text-sm pr-20'}>{description}</p>
+    <div className={'w-full md:h-screen flex px-12 py-16'}>
+      <div className={'w-1/2 flex flex-col items-center space-y-2 md:space-y-6'}>
+        <div className={'flex flex-col space-y-1 md:space-y-4 mt-0 md:mt-4'}>
+          <h1 className={'text-sm md:text-5xl font-semibold leading-none md:leading-tight'}>{heading}</h1>
+          <p className={'text-[#575757] text-[5px] md:text-sm pr-4 md:pr-20 leading-none md:leading-tight'}>{description}</p>
         </div>
         <div className={'flex'}>
           <div className={'flex flex-col'}>
-            <h1 className={'text-6xl'}>{subheading1}</h1>
-            <p className={'text-sm'}>{subcontent1}</p>
+            <h1 className={'text-sm md:text-6xl pl-2'}>{subheading1}</h1>
+            <p className={'text-[5px] md:text-sm leading-none md:leading-tight pr-4'}>{subcontent1}</p>
           </div>
           <div className={'flex flex-col'}>
-            <h1 className={'text-6xl'}>{subheading2}</h1>
-            <p className={'text-sm'}>{subcontent2}</p>
+            <h1 className={'text-sm md:text-6xl pl-2'}>{subheading2}</h1>
+            <p className={'text-[5px] md:text-sm leading-none md:leading-tight pr-4'}>{subcontent2}</p>
           </div>
         </div>
       </div>
-      <div className={'flex flex-col w-1/2 space-y-4'}>
-        <div className="relative w-full  h-2/3">
-          <Image fill src={images?.image3?.url as string} alt={images?.image3?.alt as string} className={'object-cover rounded-lg'}/>
+      <div className={'flex flex-col w-1/2 space-y-1 md:space-y-4'}>
+        <div className="relative w-full h-[84px] md:h-2/3 mt-2 md:mt-0">
+          <Image fill src={images?.image3?.url as string} alt={images?.image3?.alt as string} className={'object-cover rounded md:rounded-lg'}/>
         </div>
-        <div className={'h-1/3 flex space-x-4'}>
+        <div className={'h-[44px] md:h-1/3 flex space-x-1 md:space-x-4'}>
           <div className="relative w-full">
-            <Image fill src={images?.image2?.url as string} alt={images?.image2?.alt as string} className={'object-cover rounded-lg'}/>
+            <Image fill src={images?.image2?.url as string} alt={images?.image2?.alt as string} className={'object-cover rounded md:rounded-lg'}/>
           </div>
           <div className="relative w-full">
-            <Image fill src={images?.image1?.url as string} alt={images?.image1?.alt as string} className={'object-cover rounded-lg'}/>
+            <Image fill src={images?.image1?.url as string} alt={images?.image1?.alt as string} className={'object-cover rounded md:rounded-lg'}/>
 
           </div>
           <div className="relative w-full">
-            <Image fill src={images?.image4?.url as string} alt={images?.image4?.alt as string} className={'object-cover rounded-lg'}/>
+            <Image fill src={images?.image4?.url as string} alt={images?.image4?.alt as string} className={'object-cover rounded md:rounded-lg'}/>
           </div>
         </div>
       </div>
