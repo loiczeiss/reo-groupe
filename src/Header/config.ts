324ece19a1eb1f3c9b63d4e80c87b6e663fe778b
Loïc <loiczeiss@gmail.com>
2025-05-27 12:21:14 +0200

    fix: added revalidation to header

--- src/Header/config.ts
+++ src/Header/config.ts
@@ -1,17 +1,39 @@
-import {  GlobalConfig } from 'payload'
+import { GlobalConfig } from 'payload'
+import { revalidateGlobalHook } from '@/hooks/RevalidateGlobalHook'
 
-export const Header : GlobalConfig = {
-  slug: "header",
+export const Header: GlobalConfig = {
+  slug: 'header',
+  hooks: {
+    afterChange: [
+      (args) => {
+        console.log('[Global.afterChange] Hook fired with args:', {
+          args,
+        })
+
+        return revalidateGlobalHook({ ...args })
+      },
+    ],
+  },
   fields: [
-    {name: "logo", label: "logo", type: "upload", relationTo: "media"},
-    {name: "nav", label: "nav", type: "array", fields: [
-        {name: "navItemName", label: "navItemName", type:"text"},
-        {name:"navItemUrl", label: "navItemUrl", type: "text"},
-        {name:"navItemLogo", label: "navItemLogo", type: "upload", relationTo: "media"},
-      ]},
-    {name: "button", label: "button", type: "group", fields: [
-        {name: "label", label: "label", type: "text"},
-        {name : "url", label: "url", type: "text"},
-      ]},
-  ]
+    { name: 'logo', label: 'logo', type: 'upload', relationTo: 'media' },
+    {
+      name: 'nav',
+      label: 'nav',
+      type: 'array',
+      fields: [
+        { name: 'navItemName', label: 'navItemName', type: 'text' },
+        { name: 'navItemUrl', label: 'navItemUrl', type: 'text' },
+        { name: 'navItemLogo', label: 'navItemLogo', type: 'upload', relationTo: 'media' },
+      ],
+    },
+    {
+      name: 'button',
+      label: 'button',
+      type: 'group',
+      fields: [
+        { name: 'label', label: 'label', type: 'text' },
+        { name: 'url', label: 'url', type: 'text' },
+      ],
+    },
+  ],
 }
